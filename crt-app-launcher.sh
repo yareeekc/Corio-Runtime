549f42eb9d5a09bce8df020f86a304f6486a6f1c
Yareeek <268030883+yareeekc@users.noreply.github.com>
2026-09-19 17:33:46 +0300

    Adding Fedora- and Arch-based distros

--- crt-app-launcher.sh
+++ crt-app-launcher.sh
@@ -13,4 +13,19 @@ pkill -x "ncat"
 sleep 0.2
 /usr/bin/crt-host &
 
-chromium --app="file:///$APP_DIR/index.html"
+. /etc/os-release
+
+case "$ID" in
+    debian|ubuntu|linuxmint|pop|arch|manjaro|endeavour)
+        chromium --app="file:///$APP_DIR/index.html"
+    fedora)
+        chromium-browser --app="file:///$APP_DIR/index.html"
+    *)
+        case "$ID_LIKE" in
+            debian|ubuntu|linuxmint|pop|arch|manjaro|endeavour)
+                chromium --app="file:///$APP_DIR/index.html"
+            fedora)
+                chromium --app="file:///$APP_DIR/index.html"
+            *)
+                chromium --app="file:///$APP_DIR/index.html"
+                chromium-browser -app="file:///$APP_DIR/index.html"
